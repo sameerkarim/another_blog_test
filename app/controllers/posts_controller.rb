559d12e9974532c31c7bf4a0e88eb9ee854b4605
sameerkarim <sameerkarim1@gmail.com>
2012-03-03 13:29:37 +0800

    Added tag functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with :name => "admin", :password => "secret", :except => [:index, :show]
+  
   # GET /posts
   # GET /posts.json
   def index
